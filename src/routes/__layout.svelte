a7086f28fd2bf41fa2256f2494b88fda584b4aca
ClaudiaGiv <clau_givan@yahoo.com>
2021-06-10 11:06:10 +0300

    Add tailwindcss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '../app.postcss';
+	import '../app.css';
+	import Header from '$lib/Header/index.svelte';
+</script>
+
+<Header />
+<main>
+	<div class="h-full bg-indigo-100">
+		<div class="container px-4 mx-auto">
+			<slot />
+		</div>
+	</div>
+</main>
+<footer>
+	<div class="font-black">Woof!!</div>
+	<p class="font bold">
+		visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit
+	</p>
+</footer>
