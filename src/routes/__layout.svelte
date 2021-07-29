d337835ef6e5ad597e395e75d32bfce670d01196
ClaudiaGiv <clau_givan@yahoo.com>
2021-07-29 21:28:19 +0300

    Cleanup layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,31 +1,7 @@
-<script context="module">
-	import { createClient } from '@urql/svelte';
-
-	export async function load() {
-		const client = createClient({
-			// Pass in the fetch from sveltekit to have access to serialized requests during hydration
-			url: 'https://graphql.fauna.com/graphql',
-			fetchOptions: () => {
-				const token =
-					'Zm5BRHdGUEMtb0FDQUxlMmV2cUpMNUh1dGtJbU5kLW5oWlh0d1NoeTpQZXJzb25hbEthbmJhbjpzZXJ2ZXI=';
-				return {
-					headers: { authorization: token ? `Basic ${token}` : '' }
-				};
-			}
-		});
-		return {
-			props: { client }
-		};
-	}
-</script>
-
 <script>
 	import '../app.postcss';
 	import '../app.css';
 	import Header from '$lib/Header/index.svelte';
-	import { setClient } from '@urql/svelte';
-	export let client;
-	setClient(client)
 </script>
 
 <Header />
