2fc4a51599e27da715e8bcd8028c3102881a107d
ClaudiaGiv <clau_givan@yahoo.com>
2021-07-30 13:36:25 +0300

    Change initialization of GoTrue

--- constants/go-true.js
+++ constants/go-true.js
@@ -1,7 +1,11 @@
 import GoTrue from 'gotrue-js';
 import { NETLIFY_IDENTITY_URL } from './api-constants';
+// const GoTrue = require('gotrue-js')
 
-export const auth = new GoTrue({
-	APIUrl: NETLIFY_IDENTITY_URL,
-	setCookie: true
-});
+// export const auth = new GoTrue({
+// 	APIUrl: NETLIFY_IDENTITY_URL,
+// 	setCookie: true
+// });
+
+GoTrue.APIUrl = NETLIFY_IDENTITY_URL
+export const auth = GoTrue
