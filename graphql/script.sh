c679062bb62d86e680c24425218f466381819a92
ClaudiaGiv <clau_givan@yahoo.com>
2021-10-29 18:01:54 +0300

    Refactor the fql and json files and the bash script

--- graphql/script.sh
+++ graphql/script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#---Guide to follow first---
+#1)Log into fauna cloud locally using fauna shell
+#2)Import graphql schema in Fauna Playground for the nwly created db
+#3)Run this script
+
 if [ $# -eq 0 ]; then
   echo "No arguments supplied"
 fi
@@ -8,8 +13,10 @@ fi
 #param2 is to a file listing json/body files to execute via curl
 
 authorization=$1
-fqlSource=$2
-jsonSource=$3
+dbName=$2
+fqlSource=$3
+jsonSource=$4
+
 
 printf "\n----------Parameters:---------------------------------------------------\n\n"
 echo "Auth token: $authorization"
@@ -22,18 +29,16 @@ printf "\n 1)----------Execute fql queries--------------------------------------
 for fileName in ./$fqlSource/*; do
   #  cmd [option] "$file" >> results.out
   printf "\n----------File: $fileName -----------"
-  #eval "fauna eval Board --file=$fileName"
-  #eval "fauna eval Board --file=./fql/CreateUsers.fql"
+  eval "fauna eval $dbName --file=$fileName"
 done
 
 printf "\n\n 2)----------Execute graphql mutations--------------------------------------\n"
 
 for fileName in ./$jsonSource/*; do
   printf "\n\n----------File: $fileName -----------\n"
-#  echo -X POST 'https://graphql.fauna.com/graphql' -H 'Content-Type: application/json' -H "authorization: ${authorization}" -d ${fileName//'./'/'@'}
   curl -X POST 'https://graphql.fauna.com/graphql' -H 'Content-Type: application/json' -H "authorization: ${authorization}" -d ${fileName//'./'/'@'}
 done
-
+curl -X POST 'https://graphql.fauna.com/graphql' -H 'Content-Type: application/json' -H "authorization:Basic Zm5BRVdqM0ROLUFDUUh4UjYtMTFuRC1mbENQMExIYmp5eEQ3dTlsazpQZXJzb25hbEthbmJhbi9Cb2FyZDphZG1pbg==" -d "@json/body/create-team.json"
 ##or testing, simply replace 'curl' with 'echo'
 
 #while read source; do
