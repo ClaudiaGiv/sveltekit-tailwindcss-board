59bdaffdd72112d93c8a18d96a54687a327b7807
ClaudiaGiv <clau_givan@yahoo.com>
2021-11-01 21:46:21 +0200

    Verify if exists before fql code

--- graphql/script.sh
+++ graphql/script.sh
@@ -13,34 +13,34 @@ fi
 #param2 is to a file listing json/body files to execute via curl
 
 authorization=$1
-dbName=$2
-fqlSource=$3
-jsonSource=$4
+dbName=${2:-Board}
+fqlSource=${3:-fql}
+jsonSource=${4:-json/body}
 
 
 printf "\n----------Parameters:---------------------------------------------------\n\n"
 echo "Auth token: $authorization"
+echo "Database name: $dbName"
 echo "FQL source: $fqlSource"
 echo "JSON source: $jsonSource"
 printf "\n"
 
-printf "\n 1)----------Execute fql queries-------------------------------------------\n"
+#fauna cloud-login --secret=********** -- doesnt work in script!!!
+
+printf "\n 1)----------Import graphql schema----------------------------------------\n"
+fauna upload-graphql-schema ./gql/schema.gql #does not work yet
+
+printf "\n 2)----------Execute fql queries-------------------------------------------\n"
 
 for fileName in ./$fqlSource/*; do
-  #  cmd [option] "$file" >> results.out
   printf "\n----------File: $fileName -----------"
   eval "fauna eval $dbName --file=$fileName"
 done
 
-printf "\n\n 2)----------Execute graphql mutations--------------------------------------\n"
+printf "\n\n 3)----------Execute graphql mutations--------------------------------------\n"
 
 for fileName in ./$jsonSource/*; do
   printf "\n\n----------File: $fileName -----------\n"
   curl -X POST 'https://graphql.fauna.com/graphql' -H 'Content-Type: application/json' -H "authorization: ${authorization}" -d ${fileName//'./'/'@'}
 done
-curl -X POST 'https://graphql.fauna.com/graphql' -H 'Content-Type: application/json' -H "authorization:Basic Zm5BRVdqM0ROLUFDUUh4UjYtMTFuRC1mbENQMExIYmp5eEQ3dTlsazpQZXJzb25hbEthbmJhbi9Cb2FyZDphZG1pbg==" -d "@json/body/create-team.json"
-##or testing, simply replace 'curl' with 'echo'
 
-#while read source; do
-#   curl -X POST 'https://graphql.fauna.com/graphql' -H 'Content-Type: application/json' -H $authorization -d $source
-#done <$source
